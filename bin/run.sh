63d8e82a0148b581951436da1104f2394000e94d
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-07-05 17:37:43 -0500

    Rename script to indicate version

--- bin/run.sh
+++ bin/run.sh
@@ -4,6 +4,6 @@ CURDIR=`dirname "$0"`
 TOP=$(cd $CURDIR/.. && pwd)
 DIR=$(cd "$1" && pwd)
 
-$TOP/bin/ssw.pl workflow $DIR
+$TOP/bin/ssw-v0.pl workflow $DIR
 
-fswatch -0 -r $DIR | xargs -0 -n1 -I{} $TOP/bin/ssw.pl workflow $DIR
+fswatch -0 -r $DIR | xargs -0 -n1 -I{} $TOP/bin/ssw-v0.pl workflow $DIR
