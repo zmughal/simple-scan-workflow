dfc2de2f6079581102c264146ddaeb5769557a3f
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-08-25 09:36:45 -0500

    Turn new lines into spaces

--- lib/SSW/Process/ExtractPDFTitle/FromBeginning.pm
+++ lib/SSW/Process/ExtractPDFTitle/FromBeginning.pm
@@ -22,14 +22,18 @@ sub process {
 	$text =~ s/^\n//gm;
 
 	my ($line1, $line2) = split(/\n/, $text);
-	my ($first_n_chars) = $text =~ /((?:\s*\S){20})/m;
+	my ($first_n_chars) = $text =~ /((?:\s*\S){20})/ms;
 
 	my $line_title = $line1 && $line2 ? "$line1 $line2" : "";
 	my $char_title = $first_n_chars ? $first_n_chars : "";
 
 	my $title = min_by { length $_ }
 		grep { $_ !~ /^\s*$/ }
-		map { s/\n|(^\s+)|(\s+$)//gr }
+		map {
+			my $clean = $_ =~ s/\n/ /gr;
+			$clean =~ s/(^\s+)|(\s+$)//g;
+			$clean;
+		}
 		($line_title, $char_title);
 
 	$title ||= "";
